b2ab1a40e4d0297bb8e6d685d959a2d6c111e614
Jose Pino <jofpin@gmail.com>
2014-12-26 14:10:46 -0500

    example v1.0.2

--- assets/gumok.example.js
+++ assets/gumok.example.js
@@ -1,17 +1,13 @@
-/**
- * gumok.min.js
- * Simple template inspired by Django html syntax.
- * Version: 1.0.0
- * Github: https://github.com/mrjopino/gumok
- * Copyright, 2014 by Jose Pino, @mrjopino
- */
-$(document).ready(function() {
-var $gumok={root:function(e,t){var n=$(e).html();$(e).html($gumok.gdef(n,t))},exptemplate:/\{{|}\}/,gdef:function(e,t){var n=e.split($gumok.exptemplate);var r=n.length;var i="%c";var s="        color: #777e8c;         font-size: 1.30em;         font-weight: bold;     ";var o=1;for(;o<r;o+=2){if(t.hasOwnProperty(n[o])){n[o]=t[n[o]];console.log(i+"These using Gumok > Happy code!",s)}}var u=n.join("");return u}}
 /* App */
-  $gumok.root($("body"), {
-    "title" : "This is an example of gumok",
-    "description" : "Now create your beautiful web applications :)",
+  Gumok.view("body",{
+    "title" : "<h1>Gum<span>ok</span></h1>",
+    "description" : "Simple template inspired by Django html syntax.",
+    "subdescription" : "Has your Web applications, more beautiful with this library!",
+    "text" : "I think with gumok the world and you can make very nice web app.",
+    "author" : "Jos\u00e9 Pino",
+    "file" : "gumok.js",
+    "download" : "<a class=\"btn_download\" href=\"https://github.com/jofpin/gumok/archive/master.zip\">DOWNLOAD</a>",
+    "url.github" : "https://github.com/jofpin/gumok",
+    "url.twitter" : "https://twitter.com/jofpin"
   });
   /* End app */
-
-});
