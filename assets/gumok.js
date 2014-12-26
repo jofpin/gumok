b9e714221b1e526c6fb08de55da3e710a881edc8
Jose Pino <jofpin@gmail.com>
2014-12-26 14:13:12 -0500

    v1.0.2

--- assets/gumok.js
+++ assets/gumok.js
@@ -1,18 +1,64 @@
- /*! Gumok v1.0.1 | (c) Copyright (c) José Pino, @jofpin | https://github.com/mrjopino/gumok */
-$(document).ready(function(){var app='[core-app="'+nameApp+'"]';var $gumok={root:function(e,t){var n=$(e).html();$(e).html($gumok.gdef(n,t))},exptemplate:/\{{|}\}/,gdef:function(e,t){var n=e.split($gumok.exptemplate);var r=n.length;var i=1;for(;i<r;i+=2){if(t.hasOwnProperty(n[i])){n[i]=t[n[i]];console.log("These using Gumok > Happy code!")}}var s=n.join("");return s}}
+/**
+ *
+ * gumok.js
+ *
+ * Simple template inspired by Django html syntax.
+ * Version: 1.0.2
+ *
+ * Find the project on GitHub: 
+ * https://github.com/jofpin/gumok
+ *
+ * ============================
+ * Copyright, 2014 by Jose Pino
+ * https://twitter.com/jofpin
+ * ============================
+ */
+var Gumok = function() {
+  return {
+    reset: "",
+    regex: /\{{|}\}/,
+    credit: {
+      name: "Gumok",
+      version: "1.0.2",
+      code: "https://github.com/jofpin/gumok",
+      author: "Jose Pino",
+      twitter: "@jofpin",
+      email: "jofpin@gmail.com"
+    },
+    log: function(value) {
+      console.log(value);
+    },
+    error: function(value) {
+      console.error(value);
+    },
+    credits: function() {
+      this.log("CREDITS:" + " " + this.credit.name + " " + this.credit.version);
+      this.log("URL: " + this.credit.code);
+      this.log("------------------------------");
+      this.log("Coded by ( " + this.credit.author + ", " + this.credit.twitter + " | " + this.credit.email + ", 2014 )");
+      this.log("------------------------------");
+    },
+    view: function(element, args){
+      var body = $(element).html();
+      $(element).html(this.seting(body, args));
+    },
+    seting: function(body, data) { 
+      var unions = body.split(this.regex);
+      var count = unions.length;
+      for (var id = 1; id < count; id += 2) {
+        if (data.hasOwnProperty(unions[id])) {
+          unions[id] = data[unions[id]];
+          this.log("These using Gumok!"); 
+        } else {
+          this.error("Error in Template :(");
+        }
+      }
+      // return
+      return segs = unions.join(this.reset);
+    }
+  };
+  
+}(Gumok);
 
-/* App */
-    $gumok.root($(app), {
-    "title" : "<h1>Gum<span>ok</span></h1>",
-    "description" : "Simple template inspired by Django html syntax.",
-    "subdescription" : "Has your Web applications, more beautiful with this library!",
-    "text" : "I think with gumok the world and you can make very nice web app.",
-    "author" : "Jos\u00e9 Pino",
-    "file" : "gumok.js",
-    "download" : "<a class=\"btn_download\" href=\"https://github.com/jofpin/gumok/archive/master.zip\">DOWNLOAD</a>",
-    "url.github" : "https://github.com/jofpin/gumok",
-    "url.twitter" : "https://twitter.com/jofpin"
-  });
-  /* End app */
-
-});
+// Running credits 
+Gumok.credits();
