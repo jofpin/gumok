56ee9adbacb274fe316df42e4b99993db0f4fb5c
Jose Pino <jofpin@gmail.com>
2015-01-08 17:44:27 -0500

    v1.0.3 :leaves:

---
+++ assets/main.js
@@ -0,0 +1,15 @@
+/* scrolling for head */
+   var scrollEffect = function() {
+    $(window).scroll(function() {
+      var scrollHeader = {
+        winTop: $(window).scrollTop(),
+        size: 15
+      }
+      if(scrollHeader.winTop >= scrollHeader.size) {
+        $("header.header").addClass("sticky");
+        $(".header .master-head").addClass("sticky");
+      }
+    });
+  };
+
+scrollEffect();
